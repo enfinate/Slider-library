3fdf19b654c3536c145de849fb7a3b669fe7c706
enfinate <athershahid37@gmail.com>
2023-11-27 13:23:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,87 +10,78 @@ class Slider {
             sliderWidthPercent: 100
         }
     }) {
-
         this.duration = duration * 1000
         this.loop = autoplay
         this.looper = loop
         this.containerId = containerId
-
+        this.count = content.length
         this.currentSlide = 1
-        this.sliderNumber = content.length
+        this.sliderNumber = content.length 
+        let innerSliderWidth = content.length * 100, outerSliderWidth = advanceSetting.sliderWidthPercent
 
-        let innerSliderWidth = content.length * 100
-        let outerSliderWidth = advanceSetting.sliderWidthPercent
-
+        // Main Section ID for each instance
         this.OuterSliderId = "custom-slider-element" + Math.random()
         this.innerSliderId = "custom-slider-element-inner" + Math.random()
         this.paginationId = "pagination-area" + Math.random()
-
         let sliderContainerStructure = `
-<div 
-id="${this.OuterSliderId}" 
-style="
-width:${outerSliderWidth}%; 
-overflow: hidden
-">
-<div 
-id="${this.innerSliderId}" 
-style="
-transition: .${transition}s;
-width: ${innerSliderWidth}%; 
-display: flex; 
-position: relative; 
-left:0;
-">
-`
+            <div 
+                id="${this.OuterSliderId}" 
+                style="
+                    width:${outerSliderWidth}%; 
+                    overflow: hidden
+                ">
+                    <div 
+                        id="${this.innerSliderId}" 
+                        style="
+                            transition: .${transition}s;
+                            width: ${innerSliderWidth}%; 
+                            display: flex; 
+                            position: relative; 
+                            left:0;
+            ">
+        `
         content.forEach(e => sliderContainerStructure += e)
         sliderContainerStructure += `
-</div></div>
-<div 
-id="${this.paginationId}"
-style="
-width:100%; 
-display: flex; 
-justify-content: center; 
-margin-top:15px; 
-z-index:-1
-"></div>
-`
-
+            </div>
+            </div>
+            <div 
+                id="${this.paginationId}"
+                style="
+                    width:100%; 
+                    display: flex; 
+                    justify-content: center; 
+                    margin-top:15px; 
+                    z-index:-1
+            "></div>
+        `
         this.content = sliderContainerStructure
-
         this.totalInnerWidthPer = innerSliderWidth
         this.maxScrollBack = this.totalInnerWidthPer - 100
         this.initialLeft = 0
-
     }
 
     init({
         nav = true,
         page = true,
         pageSetting = {
-            active: `<div class="pagination-button pagination-active"></div>`,
-            inactive: `<div class="pagination-button"></div>`,
+            active: `<div class="pagination-button pagination-active" style="cursor: pointer; width:30px; height:4px; border-radius:20px; background:#666666; margin:1px"></div>`,
+            inactive: `<div class="pagination-button" style="cursor: pointer; width:30px; height:4px; border-radius:20px; background:#CBCBCB; margin:1px"></div>`,
         },
         navSetting = {
             Left: `Left`,
             Right: `Right`,
         }
     }) {
-
+        
         this.pageSettingActive = pageSetting.active
         this.pageSettingInactive = pageSetting.inactive
-
+        document.getElementById(this.containerId).style.display = "block"
         document.getElementById(this.containerId).innerHTML = this.content
-
-        let minus
-        let paginationDots
+        let minus, paginationDots
         this.loopContinue = true
-
         if (this.loop) {
             setInterval(() => {
                 if (this.loopContinue) {
-
                     if (this.initialLeft != this.maxScrollBack || this.initialLeft > this.maxScrollBack) {
                         this.initialLeft = this.initialLeft + 100
                     } else if (this.looper) {
@@ -98,16 +89,14 @@ z-index:-1
                     }
                     minus = this.initialLeft != 0 ? "-" : ""
                     document.getElementById(this.innerSliderId).style.left = minus + this.initialLeft + "%"
-
-
                     if (!(this.looper) && this.currentSlide == this.sliderNumber - 1) {
                         this.loopContinue = false
                     }
-
                     this.currentSlide = this.sliderNumber > this.currentSlide ? this.currentSlide + 1 : 1
-
                     if (page) {
+                        
                         paginationDots = ``
+                        
                         for (let i = 1; i <= this.sliderNumber; i++) {
                             if (this.currentSlide == (i)) {
                                 paginationDots += `<span id="pagi-${i}" class="everyPage">` + pageSetting.active + `</span>`
@@ -116,12 +105,12 @@ z-index:-1
                             }
                         }
                         document.getElementById(this.paginationId).innerHTML = paginationDots
+
                         this.navWorkin(page)
                     }
                 }
             }, this.duration)
         }
-
         if (page) {
             paginationDots = ``
             for (let i = 1; i <= this.sliderNumber; i++) {
@@ -134,30 +123,25 @@ z-index:-1
             document.getElementById(this.paginationId).innerHTML = paginationDots
             this.navWorkin(page)
         }
-
         if (nav) {
             let offsetHeightOfElement = document.getElementById(this.OuterSliderId).offsetHeight
-
             let randomIdLeft = "toTheLeftSlide" + Math.random();
             let randomIdRight = "toTheRightSlide" + Math.random();
-
             let containerStyle = `height:${offsetHeightOfElement}px; background: transparent; margin-top:-${offsetHeightOfElement}px; display: flex; align-items: center; padding:0 20px;`
-
             document.getElementById(this.OuterSliderId).innerHTML += `
-<div style="width:100%; height:0px; background:rgba(0,0,0,1); margin-top:0px;position: relative; z-index:20; display: flex; justify-content: space-between">
-<div style="${containerStyle}">
-<span id="${randomIdLeft}">
-${navSetting.Left}
-</span>
-</div>
-<div style="${containerStyle}">
-<span id="${randomIdRight}">
-${navSetting.Right}
-</span>
-</div>
-</div>
-`
-
+                <div style="width:100%; height:0px; background:rgba(0,0,0,1); margin-top:0px;position: relative; z-index:20; display: flex; justify-content: space-between">
+                    <div style="${containerStyle}">
+                        <span id="${randomIdLeft}">
+                            ${navSetting.Left}
+                        </span>
+                    </div>
+                    <div style="${containerStyle}">
+                        <span id="${randomIdRight}">
+                            ${navSetting.Right}
+                        </span>
+                    </div>
+                </div>
+            `
             document.getElementById(randomIdLeft).onclick = () => {
                 if (!(this.looper)) {
                     if (this.initialLeft != 0) {
@@ -180,10 +164,8 @@ ${navSetting.Right}
                             }
                             document.getElementById(this.paginationId).innerHTML = paginationDots
                             this.navWorkin(page)
-                            this.navStyleRecursion(page)
                         }
                     }
-
                 } else {
                     this.initialLeft = this.initialLeft == 0 && this.looper ? this.maxScrollBack : this.initialLeft - 100
                     minus = this.initialLeft != 0 ? "-" : ""
@@ -204,19 +186,14 @@ ${navSetting.Right}
                         }
                         document.getElementById(this.paginationId).innerHTML = paginationDots
                         this.navWorkin(page)
-                        this.navStyleRecursion(page)
                     }
                 }
             }
-
             document.getElementById(randomIdRight).onclick = () => {
-
                 if (this.loopContinue) {
-
                     if (!(this.looper) && this.currentSlide == this.sliderNumber - 1) {
                         this.loopContinue = false
                     }
-
                     this.initialLeft = this.initialLeft != this.maxScrollBack || this.initialLeft > this.maxScrollBack ? this.initialLeft + 100 : 0
                     minus = this.initialLeft != 0 ? "-" : ""
                     document.getElementById(this.innerSliderId).style.left = minus + this.initialLeft + "%"
@@ -232,28 +209,20 @@ ${navSetting.Right}
                         }
                         document.getElementById(this.paginationId).innerHTML = paginationDots
                         this.navWorkin(page)
-                        this.navStyleRecursion(page)
                     }
                 }
             }
         }
-
-        this.navStyleRecursion(page)
-
     }
 
     navWorkin(page) {
-
         if (page) {
             document.querySelectorAll(".everyPage").forEach((e) => {
                 e.onclick = () => {
-
                     e.style.cursor = `pointer`
                     let pagiSelect = parseInt((e.getAttribute("id")).split("-")[1])
-
                     this.currentSlide = pagiSelect
                     this.initialLeft = (pagiSelect * 100) - 100
-
                     let paginationDots = ``
                     for (let i = 1; i <= this.sliderNumber; i++) {
                         if (this.currentSlide == (i)) {
@@ -264,7 +233,6 @@ ${navSetting.Right}
                     }
                     document.getElementById(this.innerSliderId).style.left = "-" + this.initialLeft + "%"
                     document.getElementById(this.paginationId).innerHTML = paginationDots
-
                     if (!this.looper) {
                         if (pagiSelect == this.sliderNumber) {
                             this.loopContinue = false
@@ -272,25 +240,20 @@ ${navSetting.Right}
                             this.loopContinue = true
                         }
                     }
-                    this.navStyleRecursion(page)
                     this.recallMeasure(page)
                 }
             })
         }
-
     }
 
     recallMeasure(page) {
-
         if (page) {
             document.querySelectorAll(".everyPage").forEach((e) => {
                 e.onclick = () => {
                     e.style.cursor = `pointer`
                     let pagiSelect = parseInt((e.getAttribute("id")).split("-")[1])
-
                     this.currentSlide = pagiSelect
                     this.initialLeft = (pagiSelect * 100) - 100
-
                     let paginationDots = ``
                     for (let i = 1; i <= this.sliderNumber; i++) {
                         if (this.currentSlide == (i)) {
@@ -309,29 +272,10 @@ ${navSetting.Right}
                             this.loopContinue = true
                         }
                     }
-
                     this.navWorkin(page)
-                    this.navStyleRecursion(page)
                 }
             })
         }
-
-    }
-
-    navStyleRecursion(page) {
-
-        if (page) {
-            document.querySelectorAll(".pagination-button").forEach((e) => {
-                e.style.cursor = "pointer"
-                e.style.width = "30px"
-                e.style.height = "4px"
-                e.style.borderRadius = "20px"
-                e.style.backgroundColor = "#CBCBCB"
-                e.style.margin = "1px"
-            })
-            document.querySelector(".pagination-active").style.backgroundColor = "#666666"
-        }
-
     }
 }
 
